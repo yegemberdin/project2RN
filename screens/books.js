aa041188b2f4b1b9b5454d7a7ad1b8d465e6b5a8
«yegemberdin» <yegemberdin@gmail.com>
2018-11-26 09:36:55 +1200

    for saving const books db info

---
+++ screens/books.js
@@ -0,0 +1,329 @@
+export default [
+    {
+        author: "E L James",
+        uri: "http://omslag.boktugg.se/9789113085562.jpg?w=250",
+        title: " Fifty Shades trilogy ",
+        description: "It is the first instalment in the that traces the deepening relationship between a college graduate, Anastasia Steele, and a young business magnate, Christian Grey. It is notable for its explicitly erotic scenes featuring elements of sexual practices involving bondage/discipline, dominance/submission, and sadism/masochism (BDSM). Originally self-published as an ebook and a print-on-demand, publishing rights were acquired by Vintage Books in March 2012. ",
+        owners: [
+            {name: 'Devin', wantsFromYou: 1},
+        ],
+    },
+    {
+        author: "Niklas Natt och Dag",
+        uri: "http://omslag.boktugg.se/9789137150253.jpg?w=250",
+        title: "1793",
+        description: "In this breathtakingly bold, intricately constructed novel set in 18th century Stockholm, a dying man searches among the city’s teeming streets, dark corners, and intriguing inhabitants to unmask a ruthless murderer—perfect for fans of Perfume and The Alienist. ",
+        owners: [
+            {name: 'Jackson', wantsFromYou: 0},
+            {name: 'James', wantsFromYou: 3},
+        ],
+    },
+    {
+        author: "Dan Brown",
+        uri: "http://omslag.boktugg.se/9789100173036.jpg?w=250",
+        title: "Begynnelse",
+        description: "Robert Langdon, professor i religiös symbolik vid Harvard, anländer till Guggenheimmuseet i Bilbao för att närvara vid avslöjandet av en upptäckt som enligt inbjudan kommer att 'förändra vetenskapen för alltid'. Edmond Kirsch, en fyrtioårig miljardär som blivit världskänd för sina hypermoderna tekniska uppfinningar och djärva förutsägelser, var en gång Langdons student.",
+        owners: [
+            {name: 'Joel', wantsFromYou: 0},
+        ],
+    },
+    {
+        author: "Karin Wahlberg",
+        uri: "http://omslag.boktugg.se/9789146233534.jpg?w=250",
+        title: "Lätta ditt hjärta",
+        description: "Description not available.",
+        owners: [
+
+        ],
+    },
+    {
+        author: "Nora Roberts",
+        uri: "http://omslag.boktugg.se/9789100173357.jpg?w=250",
+        title: "Kidnapparen",
+        description: "Description not available.",
+        owners: [
+
+        ],
+    },
+    {
+        author: "Marie Hermansson",
+        uri: "http://omslag.boktugg.se/9789100169992.jpg?w=250",
+        title: "Den stora utställningen",
+        description: "Description not available.",
+        owners: [
+
+        ],
+    },
+    {
+        author: "David Baldacci",
+        uri: "http://omslag.boktugg.se/9789176295700.jpg?w=250",
+        title: "Slutspel",
+        description: "Description not available.",
+        owners: [
+            {name: 'John', wantsFromYou: 0},
+            {name: 'Jillian', wantsFromYou: 1},
+            {name: 'Jimmy', wantsFromYou: 0},
+            {name: 'Julie', wantsFromYou: 2},
+        ],
+    },
+    {
+        author: "unknown",
+        uri: "http://omslag.boktugg.se/9789100167257.jpg?w=250",
+        title: "En äkta man",
+        description: "Description not available.",
+        owners: [
+            {name: 'John', wantsFromYou: 0},
+            {name: 'Jillian', wantsFromYou: 1},
+            {name: 'Jimmy', wantsFromYou: 0},
+            {name: 'Julie', wantsFromYou: 2},
+        ],
+    },
+    {
+        author: "unknown",
+        uri: "http://omslag.boktugg.se/9789113073132.jpg?w=250",
+        title: "De kommer att drunkna i sina mödrars tårar",
+        description: "Description not available.",
+        owners: [
+            {name: 'John', wantsFromYou: 0},
+            {name: 'Jillian', wantsFromYou: 1},
+            {name: 'Jimmy', wantsFromYou: 0},
+            {name: 'Julie', wantsFromYou: 2},
+        ],
+    },
+    {
+        author: "unknown",
+        uri: "http://omslag.boktugg.se/9789137150765.jpg?w=250",
+        title: "Vågornas viskningar",
+        description: "Description not available.",
+        owners: [
+
+        ],
+    },
+    {
+        author: "unknown",
+        uri: "http://omslag.boktugg.se/9789113073736.jpg?w=250",
+        title: "Mannen som sökte sin skugga",
+        description: "Description not available.",
+        owners: [
+
+        ],
+    },
+    {
+        author: "unknown",
+        uri: "http://omslag.boktugg.se/9789164205384.jpg?w=250",
+        title: "Den fjärde pakten",
+        description: "Description not available.",
+        owners: [
+
+        ],
+    },
+    {
+        author: "unknown",
+        uri: "http://omslag.boktugg.se/9789137151755.jpg?w=250",
+        title: "Ur askan i elden",
+        description: "Description not available.",
+        owners: [
+
+        ],
+    },
+    {
+        author: "unknown",
+        uri: "http://omslag.boktugg.se/9789188545732.jpg?w=250",
+        title: "Blodlokan",
+        description: "Description not available.",
+        owners: [
+
+        ],
+    },
+    {
+        author: "unknown",
+        uri: "http://omslag.boktugg.se/9789113082738.jpg?w=250",
+        title: "Aldrig mer",
+        description: "Description not available.",
+        owners: [
+
+        ],
+    },
+    {
+        author: "unknown",
+        uri: "http://omslag.boktugg.se/9789188545671.jpg?w=250",
+        title: "Sanningen",
+        description: "Description not available.",
+        owners: [
+            {name: 'Julie', wantsFromYou: 2},
+        ],
+    },
+    {
+        author: "unknown",
+        uri: "http://omslag.boktugg.se/9789146234746.jpg?w=250",
+        title: "Återstoden av dagen",
+        description: "Description not available.",
+        owners: [
+            {name: 'Devin', wantsFromYou: 1},
+            {name: 'Julie', wantsFromYou: 2},
+        ],
+    },
+    {
+        author: "unknown",
+        uri: "http://omslag.boktugg.se/9789164205155.jpg?w=250",
+        title: "1968",
+        description: "Description not available.",
+        owners: [
+            {name: 'Devin', wantsFromYou: 1},
+            {name: 'Jimmy', wantsFromYou: 0},
+            {name: 'Julie', wantsFromYou: 2},
+        ],
+    },
+    {
+        author: "unknown",
+        uri: "http://omslag.boktugg.se/9789177791294.jpg?w=250",
+        title: "Se till mig som liten är",
+        description: "Description not available.",
+        owners: [
+            {name: 'John', wantsFromYou: 0},
+            {name: 'Jillian', wantsFromYou: 1},
+            {name: 'Jimmy', wantsFromYou: 0},
+            {name: 'Julie', wantsFromYou: 2},
+        ],
+    },
+    {
+        author: "unknown",
+        uri: "http://omslag.boktugg.se/9789100171636.jpg?w=250",
+        title: "Den svavelgula himle",
+        description: "Description not available.",
+        owners: [
+            {name: 'Devin', wantsFromYou: 1},
+            {name: 'Jackson', wantsFromYou: 0},
+            {name: 'James', wantsFromYou: 3},
+            {name: 'Joel', wantsFromYou: 0},
+        ],
+    },
+    {
+        author: "unknown",
+        uri: "http://omslag.boktugg.se/9789146229933.jpg?w=250",
+        title: "Macbeth",
+        description: "Description not available.",
+        owners: [
+            {name: 'Jimmy', wantsFromYou: 0},
+            {name: 'Julie', wantsFromYou: 2},
+        ],
+    },
+    {
+        author: "unknown",
+        uri: "http://omslag.boktugg.se/9789177710370.jpg?w=250",
+        title: "En andra chans",
+        description: "Description not available.",
+        owners: [
+            {name: 'Devin', wantsFromYou: 1},
+            {name: 'Jackson', wantsFromYou: 0},
+            {name: 'James', wantsFromYou: 3},
+            {name: 'Joel', wantsFromYou: 0},
+            {name: 'John', wantsFromYou: 0},
+            {name: 'Jillian', wantsFromYou: 1},
+            {name: 'Jimmy', wantsFromYou: 0},
+            {name: 'Julie', wantsFromYou: 2},
+        ],
+    },
+    {
+        author: "unknown",
+        uri: "http://omslag.boktugg.se/9789177950158.jpg?w=250",
+        title: "Sveas son : en berättelse om folkhemmet",
+        description: "Description not available.",
+        owners: [
+            {name: 'Julie', wantsFromYou: 2},
+        ],
+    },
+    {
+        author: "unknown",
+        uri: "http://omslag.boktugg.se/9789113069760.jpg?w=250",
+        title: "Den som stannar, den som går. Bok 3, Åren mitt i livet",
+        description: "Description not available.",
+        owners: [
+            {name: 'Devin', wantsFromYou: 1},
+            {name: 'Jackson', wantsFromYou: 0},
+        ],
+    },
+    {
+        author: "unknown",
+        uri: "http://omslag.boktugg.se/9789113076263.jpg?w=250",
+        title: "Det förlorade barnet. Bok 4 Medelålder och åldrande",
+        description: "Description not available.",
+        owners: [
+            {name: 'Julie', wantsFromYou: 2},
+        ],
+    },
+    {
+        author: "unknown",
+        uri: "http://omslag.boktugg.se/9789113082264.jpg?w=250",
+        title: "Förlåten",
+        description: "Description not available.",
+        owners: [
+            {name: 'James', wantsFromYou: 3},
+            {name: 'Jillian', wantsFromYou: 1},
+        ]
+    },
+    {
+        author: "unknown",
+        uri: "http://omslag.boktugg.se/9789137152578.jpg?w=250",
+        title: "De sju som såg",
+        description: "Description not available.",
+        owners: [
+            {name: 'Devin', wantsFromYou: 1},
+            {name: 'Julie', wantsFromYou: 2},
+        ],
+    },
+    {
+        author: "unknown",
+        uri: "http://omslag.boktugg.se/9789137150703.jpg?w=250",
+        title: "Bara du",
+        description: "Description not available.",
+        owners: [
+            {name: 'Devin', wantsFromYou: 1},
+            {name: 'Julie', wantsFromYou: 2},
+        ],
+    },
+    {
+        author: "unknown",
+        uri: "http://omslag.boktugg.se/9789127152502.jpg?w=250",
+        title: "Vaggvisa",
+        description: "Description not available.",
+        owners: [
+            {name: 'Devin', wantsFromYou: 1},
+            {name: 'Jackson', wantsFromYou: 0},
+            {name: 'James', wantsFromYou: 3},
+        ],
+    },
+    {
+        author: "unknown",
+        uri: "http://omslag.boktugg.se/9789100167134.jpg?w=250",
+        title: "Törst",
+        description: "Description not available.",
+        owners: [
+            {name: 'Devin', wantsFromYou: 1},
+            {name: 'Jackson', wantsFromYou: 0},
+            {name: 'Jimmy', wantsFromYou: 0},
+            {name: 'Julie', wantsFromYou: 2},
+        ],
+    },
+    {
+        author: "unknown",
+        uri: "http://omslag.boktugg.se/9789188545527.jpg?w=250",
+        title: "München",
+        description: "Description not available.",
+        owners: [
+            {name: 'Julie', wantsFromYou: 2},
+        ],
+    },
+    {
+        author: "unknown",
+        uri: "http://omslag.boktugg.se/9789150931099.jpg?w=250",
+        title: "Pappas pojk",
+        description: "Description not available.",
+        owners: [
+            {name: 'Devin', wantsFromYou: 1},
+            {name: 'Jackson', wantsFromYou: 0},
+            {name: 'James', wantsFromYou: 3},
+            {name: 'Joel', wantsFromYou: 0},
+        ],
+    },
+]
